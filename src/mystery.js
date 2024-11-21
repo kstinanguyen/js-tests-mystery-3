0a7d7c935743648ad3dcc72255f92b2afd4cc9f3
Kristina N <87397396+kstinanguyen@users.noreply.github.com>
2024-11-21 12:09:23 -0800

    solution passed all test suite tests

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,15 @@
-export const mystery3 = (string1, string2) => {};
+export const mystery3 = (string1, string2) => {
+  if (!string1 && !string2) {
+    return 0;
+  };
+
+  let total = 0;
+
+  for (let i = 0, j = 0; i < string1.length && j < string2.length; i++, j++) {
+    if (string1[i] !== string2[j]) {
+      total += 1;
+    }
+  }
+
+  return total;
+};
